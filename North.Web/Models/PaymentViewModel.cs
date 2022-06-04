fe61022ec0d639ded1768f3d7feb3d5b8430a6dc
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-06-04 13:53:32 +0300

    PaymentViewModel Add

---
+++ North.Web/Models/PaymentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using North.Core.Payments;
+
+namespace North.Web.Models
+{
+    public class PaymentViewModel
+    {
+        public CardModel CardModel { get; set; }
+        public List<CartModel> Carts { get; set; } = new();
+        public int Installment { get; set; }
+    }
+}
